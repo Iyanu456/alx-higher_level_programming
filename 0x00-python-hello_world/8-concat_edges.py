e34bc95fc867f0935ebf15feb0bd6ec80fae655a
Iyanu456 <oyerindei13@gmail.com>
2022-11-28 11:30:32 -0800

    Default message

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 str = "Python is an intepreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
 
 str = str[38:53] + ' ' + str[54:65] + ' ' + str[-22:-18] + ' ' + str[0:6]
