0651ae403ef4d81b593219e3858e98b87a5f2dae
Iyanu456 <oyerindei13@gmail.com>
2022-12-01 11:40:15 -0800

    Default message

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+
 import hidden_4
+
 
 def discover():
     name = dir(hidden_4)
     for i in name:
         if i[:2] != '__':
             print(f"{i}")
+
 
 if __name__ == "__main__":
     discover()
