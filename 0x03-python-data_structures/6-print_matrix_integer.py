111e176f59bfdf1ea5745850e56ddb52ddadd5fe
Iyanu456 <oyerindei13@gmail.com>
2022-12-03 15:28:38 -0800

    Default message

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,8 +5,7 @@ def print_matrix_integer(matrix=[[]]):
             for i in range(0, len(row)):
                 if i == (len(row) - 1):
                     print("{:d}".format(row[i]))
-                elif len(row) <= 0:
-                    print()
-                    break
                 else:
                     print("{:d}".format(row[i]), end=' ')
+            if (len(row) <= 0):
+                print()
