a2b90829464f9667887443d2c688360990301b5e
Iyanu456 <oyerindei13@gmail.com>
2022-12-03 14:15:03 -0800

    Default message

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,6 +4,8 @@ def no_c(my_string):
         return
     new_string = ""
     for i in my_string:
-        if i is not 'c' and i is not 'C':
+        if i == 'c' or i == 'C':
+            continue
+        else:
             new_string += i
     return (new_string)
