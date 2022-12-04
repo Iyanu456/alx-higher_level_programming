0314f8ae898b6d33f99f6d9da1eb2787c9b085e8
Iyanu456 <oyerindei13@gmail.com>
2022-12-04 13:29:31 -0800

    Default message

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    max_integer = my_list[0]
+    for i in my_list:
+        if i >= max_integer:
+            max_integer = i;
+        else:
+            max_integer = max_integer
+    return (max_integer)
