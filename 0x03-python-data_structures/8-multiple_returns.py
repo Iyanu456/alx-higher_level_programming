eba163be101b209511b16bc200fbc078338dd1bb
Iyanu456 <oyerindei13@gmail.com>
2022-12-04 12:34:51 -0800

    Default message

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,8 +2,8 @@
 def multiple_returns(sentence):
     count = 0
     word = sentence[0]
-    if sentence == None:
-        word = None
+    if sentence is None:
+        word = "None"
     for i in sentence:
         count += 1
     return (count, word)
