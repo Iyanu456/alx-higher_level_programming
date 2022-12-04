d1a3e2b048862ba8bbb7cd9d0f33a87186458d6a
Iyanu456 <oyerindei13@gmail.com>
2022-12-04 14:42:06 -0800

    Default message

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,13 +3,15 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
 operator = ['+', '-', '*', '/']
-func = [add(a, b), sub(a, b), mul(a, b), div(a, b)]
 if len(sys.argv) != 4:
     print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
     sys.exit(1)
+a = sys.argv[1]
+b = sys.argv[3]
+func = [add(a, b), sub(a, b), mul(a, b), div(a, b)]
 for i in range(0, 4):
     if sys.argv[2] == operator[i]:
-        print("{} {} {} = {}".format(int(sys.arg[1]), sys.argv[2], int(sys.argv[3]), func[i]))
+        print("{} {} {} = {}".format(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]), func[i]))
         return
     else:
         continue
