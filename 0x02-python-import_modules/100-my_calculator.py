59778d8fe13647212f9c374a15b074e2e0521182
Iyanu456 <oyerindei13@gmail.com>
2022-12-04 14:47:15 -0800

    Default message

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,12 +6,12 @@ operator = ['+', '-', '*', '/']
 if len(sys.argv) != 4:
     print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
     sys.exit(1)
-a = sys.argv[1]
-b = sys.argv[3]
+a = int(sys.argv[1])
+b = int(sys.argv[3])
 func = [add(a, b), sub(a, b), mul(a, b), div(a, b)]
 for i in range(0, 4):
     if sys.argv[2] == operator[i]:
-        print("{} {} {} = {}".format(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]), func[i]))
+        print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3], func[i]))
         sys.exit()
     else:
         continue
