b92e678fd728abaf611644f1cd0f2b82e8ca3de2
Iyanu456 <oyerindei13@gmail.com>
2022-12-20 16:43:02 +0100

    Default message

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -10,8 +10,8 @@ class Square:
         Args:
             size(int): The size of the new square.
         """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -55,11 +55,11 @@ class Square:
         if self.__size == 0:
             print("")
             return
-        else:
-            [print("") for m in range(0, self.__position[1])]
-            for i in range(0, self.__size):
-                for k in range(self.__position[0]):
-                    print(" ", end="")
-                for j in range(0, self.__size):
-                    print("#", end="")
-                print("")
+
+        [print("") for m in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            for k in range(self.__position[0]):
+                print(" ", end="")
+            for j in range(0, self.__size):
+                print("#", end="")
+            print("")
