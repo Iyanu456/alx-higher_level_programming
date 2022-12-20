2b07f663d829d247f7beb817de5bb52914457a82
Iyanu456 <oyerindei13@gmail.com>
2022-12-20 04:15:27 +0100

    Default message

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    quo = 0
+    result = 0
     try:
-        quo = a / b
-    except (ZeroDivisionError, ValueError, OverflowError):
-        quo = none
+        result = a / b
+    except (ZeroDivisionError, OverflowError):
+        result = None
     finally:
-        print("Inside result: {}".format(quo))
-        return quo
+        print("Inside result: {}".format(result))
+        return result
