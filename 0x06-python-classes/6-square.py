1678e320d0746175a5f63e0ec72b9bb7b8ecd7f6
Iyanu456 <oyerindei13@gmail.com>
2022-12-20 16:44:43 +0100

    Default message

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -58,7 +58,7 @@ class Square:
 
         [print("") for m in range(0, self.__position[1])]
         for i in range(0, self.__size):
-            for k in range(self.__position[0]):
+            for k in range(0, self.__position[0]):
                 print(" ", end="")
             for j in range(0, self.__size):
                 print("#", end="")
