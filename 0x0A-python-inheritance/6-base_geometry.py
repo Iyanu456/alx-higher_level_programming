19aec0d4f9cbc688db676b06dda7841a1601a957
Iyanu456 <oyerindei13@gmail.com>
2023-01-09 13:01:58 +0100

    default message

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""BaseGeometry class"""
+
+
+class BaseGeometry():
+    """class BaseGeometry"""
+
+    def area(self):
+        """area not implemented"""
+
+        raise Exception("area() is not implemented")
