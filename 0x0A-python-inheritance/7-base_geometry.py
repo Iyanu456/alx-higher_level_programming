ba83cbb750be83a6040df6c69926bfdf4c7d8de6
Iyanu456 <oyerindei13@gmail.com>
2023-01-09 13:37:33 +0100

    default message

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -12,7 +12,7 @@ class BaseGeometry():
             raise TypeError("{} must be an integer".format(name))
 
         if value <= 0:
-            raise ValueError("{} must be graeter than 0".format(name))
+            raise ValueError("{} must be greater than 0".format(name))
 
     def area(self):
         """area not implemented"""
