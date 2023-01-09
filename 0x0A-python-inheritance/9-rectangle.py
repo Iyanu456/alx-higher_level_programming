b47da1c839ef4478dca7a82be1f377fc4d4e74f6
Iyanu456 <oyerindei13@gmail.com>
2023-01-09 13:47:49 +0100

    default message

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Rectangle class"""
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Implements a rectangle"""
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """Calculates the area of the rectangle"""
+
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        """print"""
+
+        return ("[Rectangle] " + str(self.__width) + "/" + str(self.__height))
