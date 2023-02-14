5b9e67987f5d07a9f3c7bd389b546510faa2b9ca
Iyanu456 <oyerindei13@gmail.com>
2023-02-14 15:31:24 -0800

    default message

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- a script that creates a table called first_table in the current table
-CREATE TABLE IF NOT EXISTS first_table (id INTT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
