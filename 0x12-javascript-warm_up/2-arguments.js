c912054fde40465d2d708e65fe5d6f830055e15d
Iyanu456 <oyerindei13@gmail.com>
2023-03-13 13:16:59 +0100

    default message

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 
 // checks for command line arguments
 if (process.argv.length <= 1) {
-	console.log('No argument');
+  console.log('No argument');
 } else {
-	console.log('Argument found');
+  console.log('Argument found');
 }
