5d8546552c82471f41f0e847311760e2ee7355d3
Iyanu456 <oyerindei13@gmail.com>
2023-03-14 22:44:02 +0000

    default message

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -8,10 +8,11 @@ class Square extends Rectangle {
 
   charPrint (c) {
     if (c === undefined) {
-      c = 'X';
-    }
-    for (let i = 0; i < this.size; i++) {
-      console.log(c.repeat(this.size));
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
     }
   }
 }
