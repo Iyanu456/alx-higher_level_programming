c58951c6d545f8232f3f378de681f578f2f08d4a
Iyanu456 <oyerindei13@gmail.com>
2023-03-14 23:04:17 +0000

    default message

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,7 +2,7 @@
 
 exports.nbOccurrences = function (list, searchElement) {
   let count = 0;
-  for (const i in list) {
+  for (let i = 0; i < list.length; i++) {
     if (list[i] === searchElement) {
       count++;
     }
