030c81c143d27d77eaf5bdc462cc54e45f25e2a1
Iyanu456 <oyerindei13@gmail.com>
2023-03-18 23:32:10 +0100

    default message

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 
-
 import MySQLdb
 from sys import argv
 
-'''
-a script that lists all states
-from the database
-'''
+"""
+0-select_states.py script that lists all states from the database.
+
+It connects to an sql server with a user, password and database
+parameter
+
+"""
+
 if __name__ == "__main__":
     con = MySQLdb.connect(
-        host="localhost", port=3306, user=argv[1],
-        password=argv[2], database=argv[3])
+            host="localhost", port=3306, user=argv[1],
+            password=argv[2], database=argv[3])
     cursor = con.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
     db = cursor.fetchall()
