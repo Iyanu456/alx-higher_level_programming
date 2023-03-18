3b81ca7755612021704a1a1be6aee21d1e5e6201
Iyanu456 <oyerindei13@gmail.com>
2023-03-18 23:45:12 +0000

    default message

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+script that list all states that contain the search name
+from the database hbtn_0e_0_usa
+
+it takes in four arguments:
+mysql username, mysql password, database name and a search name
+
+get the mysql username, password, and database name from command line arguments
+connect to the MySQL server
+prepare a cursor object using cursor() method
+execute SQL query to select all states from the database
+fetch all the rows using fetchall() method
+print the results if only the state is equal to the search name
+disconnect from server
+"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    mysql_user = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=mysql_user,
+        passwd=mysql_password, db=database_name)
+    cursor = db.cursor()
+
+    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities \
+    JOIN states ON cities.state_id = states.id ORDER BY cities.id")
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+    db.close()
