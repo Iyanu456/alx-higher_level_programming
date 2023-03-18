a1dbafed1701ce3642beaa419d0938e74f276704
Iyanu456 <oyerindei13@gmail.com>
2023-03-18 23:31:08 +0000

    default message

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 """
-script that list all states with a name starting with 'N
+script that list all states that contain the search name
 from the database hbtn_0e_0_usa
 
-it takes in three arguments:
-mysql username, mysql password, and database name
+it takes in four arguments:
+mysql username, mysql password, database name and a search name
 
 get the mysql username, password, and database name from command line arguments
 connect to the MySQL server
 prepare a cursor object using cursor() method
 execute SQL query to select all states from the database
 fetch all the rows using fetchall() method
-print the results if only the state has a name that
-starts with 'N'
+print the results if only the state is equal to the search name
 disconnect from server
 """
 
