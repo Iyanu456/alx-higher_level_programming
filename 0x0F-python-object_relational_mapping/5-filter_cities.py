fe6bcba6840d51abfc57a960d525a81ecf449bc3
Iyanu456 <oyerindei13@gmail.com>
2023-03-19 20:20:28 +0100

    default message

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -20,19 +20,20 @@ import sys
 from sys import argv
 
 if __name__ == "__main__":
-	mysql_user = sys.argv[1]
-	mysql_password = sys.argv[2]
-	database_name = sys.argv[3]
+    mysql_user = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
 
-	db = MySQLdb.connect(
-	host="localhost", port=3306, user=mysql_user,
-	passwd=mysql_password, db=database_name)
-	cursor = db.cursor()
+    db = MySQLdb.connect(
+            host="localhost", port=3306, user=mysql_user,
+            passwd=mysql_password, db=database_name)
 
-	cursor.execute("SELECT cities.name FROM cities \
-	JOIN states ON cities.state_id = states.id WHERE states.name LIKE %s \
-	ORDER BY cities.id", (argv[4],))
+    cursor = db.cursor()
 
-	rows = cursor.fetchall()
-	print(", ".join(city[0] for city in rows))
-	db.close()
+    cursor.execute("SELECT cities.name FROM cities \
+    JOIN states ON cities.state_id = states.id WHERE states.name LIKE %s \
+    ORDER BY cities.id", (argv[4],))
+
+    rows = cursor.fetchall()
+    print(", ".join(city[0] for city in rows))
+    db.close()
