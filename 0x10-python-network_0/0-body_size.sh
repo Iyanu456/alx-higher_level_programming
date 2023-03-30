8ba368547e7a81efee8682b76a489e9082292979
Iyanu456 <oyerindei13@gmail.com>
2023-03-30 20:31:25 +0100

    default message

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -3,11 +3,4 @@
 # sends a request to that URL, 
 # and displays the size of the body of the response
 
-url="$1"
-
-tmpfile=$(mktemp)
-curl -sSL "$url" -o "$tmpfile"
-
-size=$(wc -c < "$tmpfile")
-echo "$size"
-rm "$tmpfile"
+curl -sI "$1" | grep "Content-Length" | cut -d " " -f2
