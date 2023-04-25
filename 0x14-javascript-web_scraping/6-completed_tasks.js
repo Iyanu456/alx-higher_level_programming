7afba86e973df05e4c690201cf60b155ab876339
Iyanu456 <oyerindei13@gmail.com>
2023-04-25 16:38:44 +0100

    default message

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,17 +2,17 @@
 // A script that counts the number of tasks completed by user id.
 
 const args = process.argv;
-let reqURL = args[2];
-let req = require('request');
+const reqURL = args[2];
+const req = require('request');
 req(reqURL, function (error, response, body) {
   if (error) {
     console.log('error:', error);
   } else {
-    let todos = JSON.parse(body);
-    let obj = {};
+    const todos = JSON.parse(body);
+    const obj = {};
     for (let i = 0; i < todos.length; i++) {
-      let status = (todos[i]['completed']);
-      let key = todos[i]['userId'].toString();
+      const status = (todos[i].completed);
+      const key = todos[i].userId.toString();
       if (status) {
         if (obj[key]) {
           obj[key]++;
