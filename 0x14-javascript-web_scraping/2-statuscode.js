10e7f9d7b9f8b50172fc1cb67a1762b35e8283de
Iyanu456 <oyerindei13@gmail.com>
2023-04-25 16:10:17 +0100

    default message

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,7 @@
 // A script that display the status code of a GET request
 
 const args = process.argv;
-let req = require('request');
+const req = require('request');
 req(args[2], function (error, response, body) {
   if (error) {
     console.log('error:', error); // Print the error if one occurred
